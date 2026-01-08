284453cc20a7c0295b52145f45e4d2b2b646c278
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2026-01-08 18:49:17 +0500

    Updated

--- app/practice/page.tsx
+++ app/practice/page.tsx
@@ -1,10 +1,11 @@
 'use client'
 import React, { useState } from 'react'
+import { useWeatherStore } from '../store/weather.store'
 
 const page = () => {
 const [city, setCity]= useState()
 
-
+const {weather,loading,error,fetchWeather}=useWeatherStore()
 
   return (
     
